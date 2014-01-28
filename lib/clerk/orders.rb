73286048252a9a2d51f4d68758937bdf15819154
Torsten Ruger <torsten@villataika.fi>
2014-01-28 23:33:27 +0200

    was falsely adding tax, removed

--- lib/clerk/orders.rb
+++ lib/clerk/orders.rb
@@ -1,27 +1,19 @@
 
 module Clerk::Orders
 
+  #deleting items that have (for inexplicable reasons) no product anymore.
   def fix_orders
     Order.all.each do |order|
       puts "order #{order.number}"
       del = false
       del = true if order.created_at.year < 2012
       order.basket.items.each do |item|
-        if item.product
-          item.price *= (100.0 + item.product.tax ) / 100.0
-          item.tax = item.product.tax
-          item.tax = 24.0 if item.tax < 0.1   #this "should" be the default system tax
-        else
-          del = true
-        end
+        del = true unless item.product
         del ? item.delete :  item.save! 
       end
       if(del)
         order.basket.delete
         order.delete
-      else
-        order.basket.save!
-        order.save!
       end
     end
   end
